bff3e24545121c11e04f46753a3448e445ec4a9f
zivce <zivkovicmilos5@hotmail.com>
2019-04-03 18:58:22 +0200

    commit 101

---
+++ SocialEvolutionSensor/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SocialEvolutionSensor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+        }
+    }
+}
