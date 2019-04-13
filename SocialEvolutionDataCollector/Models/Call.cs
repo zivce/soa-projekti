0803b1b236136e61cb9b6f06a4cd86d22b3233b4
tsopeh <5ar100kic@gmail.com>
2019-04-13 15:55:59 +0200

    Added User, SMS and Call models to DataCollectorService

---
+++ SocialEvolutionDataCollector/Models/Call.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace SocialEvolutionSensor.Models
+{
+    public class Call
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string UserId { get; set; }
+
+        [BsonElement("TimeStamp")]
+        public DateTime TimeStamp { get; set; }
+
+        [BsonElement("Duration")]
+        public int Duration { get; set; }
+
+        [BsonElement("DestUserId")]
+        public string DestUserId { get; set; }
+
+        [BsonElement("DestPhoneHash")]
+        public string DestPhoneHash { get; set; }
+    }
+}
---
+++ SocialEvolutionDataCollector/Models/SMS.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace SocialEvolutionSensor.Models
+{
+    public class SMS
+    {
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string UserId { get; set; }
+
+        [BsonElement("TimeStamp")]
+        public DateTime TimeStamp { get; set; }
+
+        [BsonElement("Incoming")]
+        public Boolean Incoming { get; set; }
+
+        [BsonElement("DestUserId")]
+        public string DestUserId { get; set; }
+
+        [BsonElement("DestPhoneHash")]
+        public string DestPhoneHash { get; set; }
+    }
+}
