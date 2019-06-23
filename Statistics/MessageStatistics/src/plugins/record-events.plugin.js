613ba84482a7f2ab670e1057346e5e5bd077b0ee
tsopeh <5ar100kic@gmail.com>
2019-06-23 14:30:00 +0200

    Untested fetch from record-events plugin

--- Statistics/MessageStatistics/src/plugins/record-events.plugin.js
+++ Statistics/MessageStatistics/src/plugins/record-events.plugin.js
@@ -1,11 +1,16 @@
+// @ts-check
+
 // TODO fix import not working for some reason
 // const { MONGO_DB_CLOUD_URI } = require('../config/db.config');
 const mongoose = require("mongoose");
-const fetch = require("node-fetch");
-const MONGO_DB_CLOUD_URI =
-  "mongodb+srv://collector_node:collector_node@socialevolutioncluster-kde4s.mongodb.net/socialEvolutionDb?retryWrites=true";
+const fetch = require("../../node_modules/node-fetch");
 
 const MessageEventModel = require("../models/message-event.model");
+
+const EventPublisherEndpoint = "localhost:34567/publish";
+
+const MONGO_DB_CLOUD_URI =
+  "mongodb+srv://collector_node:collector_node@socialevolutioncluster-kde4s.mongodb.net/socialEvolutionDb?retryWrites=true";
 
 const plugin = "message-collect";
 const recordPattern = {
@@ -32,8 +37,7 @@ function recordEvents(options) {
         res(err, null);
         return;
       }
-      // fetch post to the mqttpublisher
-      // fetch(data)
+      checkDataAndAlert(data);
       res(null, event._id);
     });
   });
@@ -41,7 +45,7 @@ function recordEvents(options) {
   seneca.add(recordPattern, (msg, res) => {
     const currentprediction = msg.predicted;
     mongoose.connect(MONGO_DB_CLOUD_URI, { useNewUrlParser: true });
-    db = mongoose.connection;
+    const db = mongoose.connection;
     db.on("error", () => {
       res("connection error", null);
     });
@@ -54,3 +58,21 @@ module.exports = {
   recordPattern,
   recordEvents
 };
+
+function checkDataAndAlert(analyticsData) {
+  // alert logic with analyticsData
+  const shouldPublishAlert = true;
+
+  if (shouldPublishAlert) {
+    const alertMsg = {
+      msgType: "messagesServerStackOverflow"
+    };
+    fetch(EventPublisherEndpoint, {
+      method: "post",
+      body: JSON.stringify(alertMsg),
+      headers: { "Content-Type": "application/json" }
+    })
+      .then(res => res.json())
+      .then(json => console.log(json));
+  }
+}
